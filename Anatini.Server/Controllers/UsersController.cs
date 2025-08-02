9c28f4ebeb5d858fa37864eb63cc9d2099232afd
David Thomsen <uniquelydave@gmail.com>
2025-08-03 08:10:27 +1200

    Start working on users controller

---
+++ Anatini.Server/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Anatini.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController() : ControllerBase
+    {
+        [HttpGet("{userHandle}/posts/{postHandle}")]
+        [ProducesResponseType<PostResponse>(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetUserPost(string userHandle, string postHandle)
+        {
+            var response = new PostResponse
+            {
+                Post = $"users/{userHandle}/posts/{postHandle}",
+            };
+
+            return Ok(response);
+        }
+    }
+
+    internal class PostResponse
+    {
+        public required string Post { get; set; }
+    }
+}
--- anatini.client/src/components/PostView.vue
+++ anatini.client/src/components/PostView.vue
@@ -1,20 +1,47 @@
-<script setup>
-  import { ref } from 'vue'
-  import { useRoute, onBeforeRouteUpdate } from 'vue-router'
+<script setup lang="ts">
+  import { ref, watch } from 'vue'
+  import { useRoute } from 'vue-router'
 
   const route = useRoute();
 
-  const userHandle = ref($route.params.userHandle);
-  const postHandle = ref($route.params.postHandle);
+  const loading = ref<boolean>(false)
+  const post = ref<string | null>(null)
+  const error = ref<string | null>(null)
 
-  onBeforeRouteUpdate(async (to, from) => {
-    userHandle.value = to.params.userHandle;
-    postHandle.value = to.params.postHandle;
-  })
+  watch([() => route.params.userHandle, () => route.params.postHandle], fetchPost, { immediate: true })
+
+  async function fetchPost(array: (() => string | string[])[]) {
+    error.value = post.value = null
+    loading.value = true
+
+    fetch(`/api/users/${array[0]}/posts/${array[1]}`, { method: "GET" })
+      .then(value => {
+        if (value.ok) {
+          value.json()
+            .then(value => {
+              post.value = value.post;
+            })
+            .catch(() => {
+              error.value = 'Unknown Error';
+            });
+        } else if (value.status === 401) {
+          error.value = 'unauthorised';
+        } else {
+          error.value = 'internal server error';
+        }
+      })
+      .catch(() => {
+        error.value = 'Unknown Error';
+      }).
+      finally(() => {
+        loading.value = false
+      });
+  }
 </script>
 
 <template>
   <h2>PostView</h2>
-  <p>User {{ userHandle }}</p>
-  <p>Post {{ postHandle }}</p>
+  <p v-if="loading">Loading...</p>
+  <p v-if="error">{{ error }}</p>
+  <p v-if="post">{{ post }}</p>
 </template>
