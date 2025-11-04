2eb88851cf37665efdca1a8c5a64e6520f81193d
David Thomsen <uniquelydave@gmail.com>
2025-11-05 06:39:29 +1300

    Generate HTML

--- anatini.client/src/components/ContentPreviewView.vue
+++ anatini.client/src/components/ContentPreviewView.vue
@@ -2,7 +2,7 @@
   import { ref, watch } from 'vue';
   import { useRoute } from 'vue-router';
   import { apiFetch } from './common/apiFetch';
-  import type { Content } from '@/types';
+  import type { Content, ContentElement } from '@/types';
 
   const route = useRoute();
 
@@ -24,25 +24,37 @@
 
     await apiFetch(`channels/${array[0]}/contents/${array[1]}/preview`, onfulfilled, onfinally);
   }
+
+  function sanitizeElementContent(elementContent: string): string {
+    return elementContent
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
+  }
+
+  function getContents(elements: ContentElement[]): string {
+    let result = '';
+
+    elements.sort((a, b) => a.index - b.index).forEach(element => {
+      if (element.content !== null)
+      {
+        result += `<${element.tag}>${sanitizeElementContent(element.content)}</${element.tag}>`;
+      }
+    });
+
+    return result;
+  }
+
+  function getMainHtml(): string {
+    if (loading.value) {
+      return '<p>Loading...</p>';
+    } else if (content.value?.version.elements !== undefined) {
+      return getContents(content.value.version.elements);
+    } else {
+      return '<b>Error</b>';
+    }
+  }
 </script>
 
 <template>
-  <main>
-    <p v-if="loading">Loading...</p>
-    <template v-else-if="content">
-      <template v-if="content.version.elements">
-        <template v-for="element in content.version.elements.sort((a, b) => a.index > b.index ? 1 : -1)" :key="'element' + element.index">
-          <template v-if="element.content !== null">
-            <h1 v-if="element.tag == 'h1'">{{ element.content }}</h1>
-            <h2 v-else-if="element.tag == 'h2'">{{ element.content }}</h2>
-            <h3 v-else-if="element.tag == 'h3'">{{ element.content }}</h3>
-            <h4 v-else-if="element.tag == 'h4'">{{ element.content }}</h4>
-            <h5 v-else-if="element.tag == 'h5'">{{ element.content }}</h5>
-            <h6 v-else-if="element.tag == 'h6'">{{ element.content }}</h6>
-            <p v-else-if="element.tag == 'p'">{{ element.content }}</p>
-          </template>
-        </template>
-      </template>
-    </template>
-  </main>
+  <main v-html="getMainHtml()"></main>
 </template>
