1c85ba12e2c8e5c463fcfe06952d2f4cfee3abbd
David Thomsen <uniquelydave@gmail.com>
2025-12-31 08:28:26 +1300

    Improve style and accessibility of user page

--- anatini.client/src/components/UserView.vue
+++ anatini.client/src/components/UserView.vue
@@ -6,88 +6,79 @@
 
   const route = useRoute();
 
-  const loading = ref<boolean>(true);
-  const error = ref<string | null>(null);
+  const fetching = ref<boolean>(true);
   const user = ref<User | null>(null);
+  const error = ref<{ header: string, body: string } | null>(null);
 
   watch([() => route.params.userId], fetchUser, { immediate: true });
 
   async function fetchUser(array: (() => string | string[])[]) {
+    fetching.value = true;
     error.value = user.value = null;
-    loading.value = true;
 
     const onfulfilled = async (value: User) => {
       user.value = value;
     };
 
     const onfinally = () => {
-      loading.value = false;
+      fetching.value = false
     };
 
-    apiFetch(`users/${array[0]}`, onfulfilled, onfinally);
+    const statusActions = {
+      404: () => {
+        error.value = {
+          header: '404 Not Found',
+          body: 'This user cannot be found',
+        };
+      }
+    };
+
+    apiFetch(`users/${array[0]}`, onfulfilled, onfinally, undefined, statusActions);
   }
 
-  async function trust() {
-    if (user.value === null) {
-      return;
+  function getHeading(): string {
+    if (fetching.value) {
+      return 'Fetching...';
+    } if (user.value) {
+      return user.value.name;
+    } else if (error.value) {
+      return error.value.header;
+    } else {
+      return 'Unknown Error';
     }
-
-    const onfulfilled = async (value: string) => {
-      console.log(value);
-    };
-
-    const onfinally = () => {
-      loading.value = false;
-    };
-
-    const body = new FormData();
-
-    body.append('type', 'Trusts');
-
-    const init = { method: "POST", body: body };
-
-    apiFetch(`users/${user.value.id}/relationships`, onfulfilled, onfinally, init);
   }
 </script>
 
 <template>
-  <main v-if="user">
-    <article>
-      <header aria-busy="false">
+  <main>
+    <article :aria-busy="fetching ? 'true' : 'false'" aria-live="polite" aria-labelledby="heading">
+      <header>
         <figure>
-          <img v-if="user.iconImageUri" alt="Image" :src="user.iconImageUri" width="400" height="400" />
-          <figcaption>Member since 2023</figcaption>
+          <img v-if="user?.iconImageUri" alt="Image" :src="user.iconImageUri" width="400" height="400" />
+          <svg v-else
+            view-box="0 0 400 400"
+            width="400"
+            height="400">
+            <rect width="400" height="400" fill="#f0f" />
+          </svg>
+          <figcaption>Picture Of User</figcaption>
         </figure>
 
-        <h1>{{ user.name }}</h1>
+        <h1 id="heading">{{ getHeading() }}</h1>
       </header>
 
-      <section aria-label="User biography">
-        <p>User biography goes here</p>
-      </section>
-
-      <footer>
-        <menu type="toolbar">
-          <li>
-            <button type="button" aria-pressed="false" @click="() => trust()">Trust</button>
-          </li>
-          <li>
-            <button type="button" class="danger">Block</button>
-          </li>
-        </menu>
-      </footer>
-    </article>
-  </main>
-  <main v-else>
-    <article aria-busy="true" aria-live="polite" aria-labelledby="loading-title">
-      <header>
-        <h1 id="loading-title">Loading user...</h1>
-      </header>
-
-      <section>
+      <section v-if="fetching">
         <p role="status">Please wait while the user information is fetched.</p>
                 
         <progress max="100">Loading...</progress>
+      </section>
+      <section v-else-if="user" aria-label="User biography">
+        <p>User biography goes here</p>
+      </section>
+      <section v-else>
+        <p>
+          {{error?.body ?? 'Unknown Error'}}
+        </p>
       </section>
     </article>
   </main>
