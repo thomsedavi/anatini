2e9bb8302bee72977aae298efea1f6b8cb6037b3
David Thomsen <uniquelydave@gmail.com>
2026-04-06 15:24:43 +1200

    Channel changes

--- Anatini.Server/Context/Entities/Channel.cs
+++ Anatini.Server/Context/Entities/Channel.cs
@@ -1,57 +1,36 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace Anatini.Server.Context.Entities
+﻿namespace Anatini.Server.Context.Entities
 {
-    public class Channel : BaseEntity
+    public class Channel : Entity
     {
         public required string Name { get; set; }
+        public string? Handle { get; set; }
+        public string? NormalizedHandle { get; set; }
         public string? About { get; set; }
-        public required ICollection<ChannelOwnedUser> Users { get; set; }
-        public required ICollection<ChannelOwnedAlias> Aliases { get; set; }
-        public required string DefaultHandle { get; set; }
-        public string? IconImageId { get; set; }
-        public string? BannerImageId { get; set; }
-        public string? DefaultCardImageId { get; set; }
-        public bool? Protected { get; set; }
+        public Guid? IconImageId { get; set; }
+        public Guid? BannerImageId { get; set; }
+        public Guid? DefaultCardImageId { get; set; }
+        public required string Visibility { get; set; }
+
+        public virtual ICollection<ApplicationUserChannel> UserChannels { get; set; } = [];
+        public virtual ICollection<ChannelHandle> Handles { get; set; } = [];
     }
 
-    [Owned]
-    public class ChannelOwnedUser : ChannelOwnedEntity
-    {
-        public required string Id { get; set; }
-        public required string Name { get; set; }
-    }
-
-    [Owned]
-    public class ChannelOwnedAlias : ChannelOwnedEntity
+    public class ChannelHandle : Entity
     {
         public required string Handle { get; set; }
+        public required string NormalizedHandle { get; set; }
+
+        public required Guid ChannelId { get; set; }
+        public virtual Channel Channel { get; set; } = null!;
     }
 
-    public class ChannelAlias : AliasEntity
+    public class ChannelImage : Entity
     {
-        public required string ChannelId { get; set; }
-        public required string ChannelName { get; set; }
-        public string? IconImageId { get; set; }
-        public string? BannerImageId { get; set; }
-        public string? DefaultCardImageId { get; set; }
-        public bool? Protected { get; set; }
-    }
-
-    public class ChannelImage : ChannelEntity
-    {
-        public required string Id { get; set; }
         public required string BlobContainerName { get; set; }
         public required string BlobName { get; set; }
-    }
+        public string? AltText { get; set; }
 
-    public abstract class ChannelEntity : Entity
-    {
-        public required string ChannelId { get; set; }
-    }
-
-    public abstract class ChannelOwnedEntity
-    {
-        public required string ChannelId { get; set; }
+        public required Guid ChannelId { get; set; }
+        public virtual Channel Channel { get; set; } = null!;
     }
 }
