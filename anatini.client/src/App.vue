cdcade7e46a41e8145e48dcc40fc1b55fa0bab65
David Thomsen <uniquelydave@gmail.com>
2026-06-07 08:51:22 +1200

    Add labels to link button

--- anatini.client/src/App.vue
+++ anatini.client/src/App.vue
@@ -97,8 +97,9 @@
               :aria-expanded="isShowing.includes('main')" 
               aria-controls="main-dropdown"
               @click.stop="toggleShow('main')"
+              aria-label="Open main menu"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" focusable="false">
                 <path d="M3 6h18M3 12h18M3 18h18" />
               </svg>
             </button>
